cb8f7f1269bd2f1c2255cf02173e091027b09a3f
Michael <masuggs515@gmail.com>
2021-02-13 16:26:28 -0500

    added sessions to Python

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -9,7 +9,7 @@ debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
-responses = []
+
 
 @app.route('/')
 def home_page():
@@ -17,18 +17,23 @@ def home_page():
 
 @app.route('/begin', methods=["POST"])
 def redirect_to_start():
-    responses.clear()
-    return redirect('/questions/0')
+    if session.get("responses", False):
+        if len(session["responses"]) == len(satisfaction_survey.questions):
+            return redirect('/complete')
+    else:
+        session["responses"] = []
+        return redirect('/questions/0')
 
 @app.route('/questions/<int:num>')
 def question_pages(num):
-    if (responses is None):
-        return redirect('/')
+    responses = session['responses']
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect('/complete')
     if len(responses) != num:
         if len(responses) == 0:
             flash(f'You have not started the survey yet.', 'error')
             return redirect('/')
-        flash(f'Invalid question of id: {num} You are on question {len(responses)}.', 'error')
+        flash(f'Invalid questions request. You are on question {len(responses) +1}.', 'error')
         return redirect(f'/questions/{len(responses)}')
     question = satisfaction_survey.questions[num]
     return render_template('questions.html', question = question)
@@ -40,7 +45,10 @@ def add_answer():
 
     answer = request.form['answer']
 
+    responses = session['responses']
+
     responses.append(answer)
+    session['responses'] = responses
     if len(responses) == len(satisfaction_survey.questions):
         return redirect('/complete')
     return redirect(f'/questions/{len(responses)}')
@@ -48,4 +56,4 @@ def add_answer():
 
 @app.route('/complete')
 def done_with_survey():
-    return render_template('complete.html', responses=responses)
+    return render_template('complete.html',question=satisfaction_survey.questions , responses=session['responses'])
